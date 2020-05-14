e0a5f44420b5c687260c95fff0e602418fcb1abd
your name <jackwillee1995@gmail.com>
2020-05-14 22:50:58 +0200

    added card class and test

---
+++ rspec-course/lib/card.rb
@@ -0,0 +1,10 @@
+class Card 
+      attr_reader :type #makes type attribute available to be accessed
+        def initialize(type) #when a new card instance is created it is initialized with type attribute
+        @type = type  
+            
+        end
+
+
+
+end 
