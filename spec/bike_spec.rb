71158636fa061eecbf25b8954bcedd467f1a616b
jack.willee <jackwillee1995@gmail.com>
2021-03-10 22:01:52 +0100

    added test and defined working? method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,11 @@
 require 'bike.rb'
 describe Bike do 
 
+    it 'instances of bike should be able to respond to working? method' do 
+    
+    expect(subject).to respond_to(:working?)
 
 
+    end 
 end 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,5 @@ it 'instances should respond to release_bike method' do
  expect(subject).to respond_to(:release_bike)
  
 end 
+
 end 
